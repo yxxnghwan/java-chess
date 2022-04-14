82f91eb956c1ff7e9fe691c5901e7ee3c90a25d8
sun <syoun602@gmail.com>
2022-04-14 21:54:08 +0900

    chore: 테스트 코드 DisplayName 추가
    Co-authored-by:yxxnghwan <younghwan960@gmail.com>

--- src/test/java/chess/piece/KnightTest.java
+++ src/test/java/chess/piece/KnightTest.java
@@ -4,11 +4,13 @@ import chess.piece.detail.Direction;
 import chess.piece.detail.Team;
 import chess.square.Square;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class KnightTest {
 
+    @DisplayName("나이트가 움직일 수 있는지 확인한다.")
     @ParameterizedTest
     @CsvSource({"a1,NNE", "a1,ENE", "a2,ESE", "a3,SSE", "c2,WSW", "c1,WNW", "b1,NNW"})
     void canMove(final String rawSquare, final String rawDirection) {
