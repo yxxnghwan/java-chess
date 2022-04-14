762f8fb7bfedda70b3769ec2349dcae42470fbf0
sun <syoun602@gmail.com>
2022-04-14 21:52:32 +0900

    feat: 킹이 움직일 수 있는지 확인하는 기능 구현
    Co-authored-by:yxxnghwan <younghwan960@gmail.com>

---
+++ src/test/java/chess/piece/KingTest.java
@@ -0,0 +1,22 @@
+package chess.piece;
+
+import chess.piece.detail.Direction;
+import chess.piece.detail.Team;
+import chess.square.Square;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class KingTest {
+
+    @ParameterizedTest
+    @CsvSource({"a1,N", "a1,NE", "a1,E", "a2,SE", "a2,S", "b2,SW", "b1,NW"})
+    void canMove(final String rawSquare, final String rawDirection) {
+        final Square from = Square.from(rawSquare);
+        final King king = new King(Team.WHITE, from);
+        final Direction direction = Direction.valueOf(rawDirection);
+        final Square to = from.next(direction);
+
+        Assertions.assertThat(king.canMove(to)).isTrue();
+    }
+}
