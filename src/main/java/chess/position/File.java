a566c97e129fc7aa54072b4ce4d114f279eb7814
sun <syoun602@gmail.com>
2022-04-14 15:50:40 +0900

    feat: 체스 파일 도메인 구현 및 테스트
    Co-authored-by: yxxnghwan <younghwan960@gmail.com>

---
+++ src/main/java/chess/position/File.java
@@ -0,0 +1,32 @@
+package chess.position;
+
+import java.util.Arrays;
+
+public enum File {
+    A("a"),
+    B("b"),
+    C("c"),
+    D("d"),
+    E("e"),
+    F("f"),
+    G("g"),
+    H("h"),
+    ;
+
+    private final String value;
+
+    File(final String value) {
+        this.value = value;
+    }
+
+    public static File from(final String value) {
+        return Arrays.stream(values())
+                .filter(file -> file.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 파일입니다."));
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/chess/position/FileTest.java
@@ -0,0 +1,38 @@
+package chess.position;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class FileTest {
+
+    @DisplayName("파일 이름으로 자신의 알파벳을 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"A,a", "B,b", "C,c", "D,d", "E,e", "F,f", "G,g", "H,h"})
+    void getRank(final String name, final String value) {
+        File enumName = File.valueOf(name);
+        assertThat(enumName.getValue()).isEqualTo(value);
+    }
+
+    @DisplayName("숫자로 자신의 파일를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"A,a", "B,b", "C,c", "D,d", "E,e", "F,f", "G,g", "H,h"})
+    void of(final String name, final String value) {
+        File file = File.from(value);
+        File enumName = File.valueOf(name);
+        assertThat(file).isSameAs(enumName);
+    }
+
+    @DisplayName("허용되지 않은 알파벳으로 파일을 생성할 시 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"i", "z"})
+    void invalidRankException(final String value) {
+        Assertions.assertThatThrownBy(() -> File.from(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 파일입니다.");
+    }
+}
