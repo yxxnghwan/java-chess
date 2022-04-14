0edfaebccae3e6f9763d3d47d1d77de370e57c66
sun <syoun602@gmail.com>
2022-04-14 15:36:14 +0900

    feat: 체스 랭크 도메인 구현 및 테스트
    Co-authored-by: yxxnghwan <younghwan960@gmail.com>

---
+++ src/main/java/chess/position/Rank.java
@@ -0,0 +1,32 @@
+package chess.position;
+
+import java.util.Arrays;
+
+public enum Rank {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8)
+    ;
+
+    private final int value;
+
+    Rank(final int value) {
+        this.value = value;
+    }
+
+    public static Rank from(final int value) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.value == value)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 랭크입니다."));
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/chess/position/RankTest.java
@@ -0,0 +1,38 @@
+package chess.position;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RankTest {
+
+    @DisplayName("랭크 이름로 자신의 숫자를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"ONE,1", "TWO,2", "THREE,3", "FOUR,4", "FIVE,5", "SIX,6", "SEVEN,7", "EIGHT,8"})
+    void getRank(final String name, final int value) {
+        Rank enumName = Rank.valueOf(name);
+        assertThat(enumName.getValue()).isEqualTo(value);
+    }
+
+    @DisplayName("숫자로 자신의 랭크를 반환한다.")
+    @ParameterizedTest
+    @CsvSource({"ONE,1", "TWO,2", "THREE,3", "FOUR,4", "FIVE,5", "SIX,6", "SEVEN,7", "EIGHT,8"})
+    void of(final String name, final int value) {
+        Rank rank = Rank.from(value);
+        Rank enumName = Rank.valueOf(name);
+        assertThat(rank).isSameAs(enumName);
+    }
+
+    @DisplayName("허용되지 않은 숫자로 랭크를 생성할 시 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 9})
+    void invalidRankException(final int value) {
+        Assertions.assertThatThrownBy(() -> Rank.from(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 랭크입니다.");
+    }
+}
